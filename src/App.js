ac9e4d6bbd2c1f452fc4111176c895f80ff50af9
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-02-22 03:16:11 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css'
 
-function App () {
-  return (
-    <div>
-        <h1>Hello world</h1>
+class App extends Component {
+  render(){
+  return(
+    <div className="foo">
+      <h1>Hello world</h1>
+      <label>bar:</label>
+      <input
+        type="text"
+        title="onClick"
+        onChange={() => {
+        console.log("test");
+      }}
+      />
     </div>
   )
+    }
 }
 
 export default App;
