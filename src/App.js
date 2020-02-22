cf1ced835d85f6516e5d87e6a4c95e796fbf189c
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-02-22 12:46:33 +0900

    propsのお勉強

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
 import React from 'react'
 import './App.css'
 
-// class App extends Component {
-//   render(){
-//   return(
-//     <div className="wrapper">
-//       <h1>Hello world</h1>
-//       <label>bar:</label>
-//       <input
-//         type="text"
-//         title="onClick"
-//         onChange={(text) => {
-//         console.log(text);
-//       }}
-//       />
-//     </div>
-//   )
-//     }
-// }
-
 const App = () => {
+  const profiles = [
+    {
+      name: "cat",
+      age: 10,
+    },
+    {
+      name: "hanako",
+      age: 11,
+    },
+    {
+      name: "taro",
+      age: 15,
+    },
+    {
+      
+      
+    }
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((data, index) => {
+          return (
+            <Cat
+              name={data.name}
+              age={data.age}
+              key={index}
+            />
+          );
+        })
+      }
     </div>
   );
 }
 
-const Cat = () => {
+const Cat = (props) => {
   return (
     <div>
       Meow!
+    I am {props.name}. {props.age} years old
     </div>
   );
+}
+
+Cat.defaultProps = {
+  name:'defaultName',
+  age:1
 }
 
 export default App;
