b8a82a706ccf8962d1e35454e064d55ac706c599
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-02-22 12:34:58 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './App.css'
 
-class App extends Component {
-  render(){
-  return(
-    <div className="foo">
-      <h1>Hello world</h1>
-      <label>bar:</label>
-      <input
-        type="text"
-        title="onClick"
-        onChange={() => {
-        console.log("test");
-      }}
-      />
+// class App extends Component {
+//   render(){
+//   return(
+//     <div className="wrapper">
+//       <h1>Hello world</h1>
+//       <label>bar:</label>
+//       <input
+//         type="text"
+//         title="onClick"
+//         onChange={(text) => {
+//         console.log(text);
+//       }}
+//       />
+//     </div>
+//   )
+//     }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </div>
-  )
-    }
+  );
+}
+
+const Cat = () => {
+  return (
+    <div>
+      Meow!
+    </div>
+  );
 }
 
 export default App;
