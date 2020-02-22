045925bf2da7660c9e5a6b5f169074dddcf7ae51
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-02-22 13:19:15 +0900

    stateの使い方

--- src/App.js
+++ src/App.js
@@ -1,62 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import PropTypes from 'prop-types';
 
 const App = () => {
-  const profiles = [
-    {
-      name: "cat",
-      age: 10,
-    },
-    {
-      name: "hanako",
-      age: 11,
-    },
-    {
-      name: "taro",
-      age: 15,
-    },
-    {
-      name:"satake",
-      age:17,
-    }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((data, index) => {
-          return (
-            <Cat
-              name={data.name}
-              age={data.age}
-              key={index}
-            />
-          );
-        })
-      }
-    </div>
-  );
+	return <Counter />;
+};
+
+class Counter extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			counter: 0
+		};
+	}
+
+	_plusButton = () => {
+		this.setState({ counter: this.state.counter + 1 });
+	};
+
+	_minusButton = () => {
+		this.setState({ counter: this.state.counter - 1 });
+	};
+
+	render() {
+		return (
+			<div>
+				<div>count : {this.state.counter}</div>
+				<button
+					onClick={() => {
+						this._plusButton();
+					}}
+				>
+					+1
+				</button>
+				<button
+					onClick={() => {
+						this._minusButton();
+					}}
+				>
+					-1
+				</button>
+			</div>
+		);
+	}
 }
-
-const Cat = (props) => {
-  return (
-    <div>
-      Meow!
-    I am {props.name}. {props.age} years old
-    </div>
-  );
-};
-
-
-Cat.defaultProps = {
-  name: 'defaultName',
-  age: 1
-};
-
-
-Cat.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number
-};
 
 export default App;
