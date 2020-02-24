b39fa4ae44d3b5bf31e473fa12aef37c10c0a4ad
SatakeYusuke19920527 <satkyusk@icloud.com>
2020-02-24 11:20:36 +0900

    axiosと_.map追加

--- src/components/EventsIndex.js
+++ src/components/EventsIndex.js
@@ -1,33 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {increment, decrement} from '../actions/index';
+import { readEvents } from '../actions/index';
+import _ from 'lodash';
 
 class EventsIndex extends Component {
+	componentDidMount = () => {
+		this.props.readEvents();
+	};
+
+	renderEvents = () => {
+		return _.map(this.props.events, (event) => (
+			<tr key={event.id}>
+				<td>{event.id}</td>
+				<td>{event.title}</td>
+				<td>{event.body}</td>
+			</tr>
+		));
+	};
+
 	render() {
-		const props = this.props
 		return (
-			<React.Fragment>
-				<div>value : {props.value}</div>
-				<button
-					onClick={() => {
-						props.increment()
-					}}
-				>
-					+1
-				</button>
-				<button
-					onClick={() => {
-						props.decrement()
-					}}
-				>
-					-1
-				</button>
-			</React.Fragment>
+			<table>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Title</th>
+						<th>Body</th>
+					</tr>
+				</thead>
+				<tbody>{this.renderEvents()}</tbody>
+			</table>
 		);
 	}
 }
 
-const mapStateToProps = state => ({ value: state.count.value });
-const mapDispatchToProps = ({ increment, decrement });
+const mapStateToProps = (state) => ({ events: state.events });
+const mapDispatchToProps = { readEvents };
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import EventsIndex from './components/EventsIndex';
 import './index.css';
 import reducer from './reducers';
 import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
 	<Provider store={store}>
